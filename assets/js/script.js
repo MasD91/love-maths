482e01ca6e5fedacf04f802d28939cca239f5415
MasD91 <masardauti1@outlook.com>
2023-04-04 11:10:23 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
 
     }
+
+    runGame("addition");
 })
 
 /**
  * the main game "loop", called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+    displayAdditionQuesiton(num1, num2);
+    } else {
+        alert(`unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -42,7 +51,11 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuesiton() {
+function displayAdditionQuesiton(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
 
